30934c9986c65aaaa7b3f7f971fbfd122c07fa2a
DThaiPome <ryan.gehrlein@gmail.com>
2026-08-02 20:45:41 -0400

    Add project files.

---
+++ Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(PMW2RPArchipelagoClientMod.Core), "PMW2RPArchipelagoClientMod", "1.0.0", "DThaiPome", null)]
+[assembly: MelonGame("Bandai Namco Entertainment Inc.", "PAC-MAN WORLD 2 Re-PAC")]
+
+namespace PMW2RPArchipelagoClientMod
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
