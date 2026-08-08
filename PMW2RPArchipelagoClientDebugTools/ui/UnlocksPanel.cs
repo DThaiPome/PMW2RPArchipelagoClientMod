e2777cdaea88868f6d8044390d13db2460bbfc34
DThaiPome <ryan.gehrlein@gmail.com>
2026-08-07 22:21:20 -0400

    Better debug layout for unlocks

--- PMW2RPArchipelagoClientDebugTools/ui/UnlocksPanel.cs
+++ PMW2RPArchipelagoClientDebugTools/ui/UnlocksPanel.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 using UniverseLib.UI;
 using UniverseLib.UI.Panels;
 using PMW2RPArchipelagoClientMod.models.data;
+using Il2Cpp;
 
 namespace PMW2RPArchipelagoClientDebugTools.ui
 {
@@ -19,6 +20,10 @@ namespace PMW2RPArchipelagoClientDebugTools.ui
         private Toggle _dashToggle;
         private Toggle _bombToggle;
 
+        private GameObject _uiRoot;
+
+        private Dictionary<EWorldStage, Toggle> _stageToggles = new Dictionary<EWorldStage, Toggle>();
+
         public UnlocksPanel(UIBase owner) : base(owner)
         {
 
@@ -26,9 +31,9 @@ namespace PMW2RPArchipelagoClientDebugTools.ui
 
         public override string Name => "Toggle Unlocks";
 
-        public override int MinWidth => 300;
+        public override int MinWidth => 600;
 
-        public override int MinHeight => 600;
+        public override int MinHeight => 1050;
 
         public override Vector2 DefaultAnchorMin => new Vector2(0f, 0f);
 
@@ -36,11 +41,21 @@ namespace PMW2RPArchipelagoClientDebugTools.ui
 
         protected override void ConstructPanelContent()
         {
-            ConstructToggle("buttBounce1", "Progressive Butt-Bounce", out _buttBounceToggle1);
-            ConstructToggle("buttBounce2", "Progressive Butt-Bounce", out _buttBounceToggle2);
-            ConstructToggle("kick", "Flip Kick", out _kickToggle);
-            ConstructToggle("dash", "Dash", out _dashToggle);
-            ConstructToggle("bomb", "Pac-Dot Throw", out _bombToggle);
+            _uiRoot = UIFactory.CreateUIObject("unlockPanelRoot", ContentRoot);
+            UIFactory.SetLayoutGroup<HorizontalLayoutGroup>(_uiRoot, childControlWidth: true, childControlHeight: true, forceWidth: true, forceHeight: true);
+            _constructMovesetToggles();
+            _constructStageToggles();
+        }
+
+        private void _constructMovesetToggles()
+        {
+            var columnObj = UIFactory.CreateUIObject("movesetColumn", _uiRoot);
+            UIFactory.SetLayoutGroup<VerticalLayoutGroup>(columnObj, childControlWidth: true, childControlHeight: true, forceWidth: true, forceHeight: false);
+            _constructToggle(columnObj, "buttBounce1", "Progressive Butt-Bounce", out _buttBounceToggle1);
+            _constructToggle(columnObj, "buttBounce2", "Progressive Butt-Bounce", out _buttBounceToggle2);
+            _constructToggle(columnObj, "kick", "Flip Kick", out _kickToggle);
+            _constructToggle(columnObj, "dash", "Dash", out _dashToggle);
+            _constructToggle(columnObj, "bomb", "Pac-Dot Throw", out _bombToggle);
 
             var debugUnlocksService = PMW2RPArchipelagoClientMod.services.ServiceFactory.GetDebugUnlocksService();
             _kickToggle.isOn = debugUnlocksService.FlipKick;
@@ -51,15 +66,35 @@ namespace PMW2RPArchipelagoClientDebugTools.ui
             _buttBounceToggle2.isOn = debugUnlocksService.ButtBounce == ProgressiveButtBounce.SuperButtBounce;
         }
 
-        private void ConstructToggle(string name, string label, out Toggle toggle)
+        private void _constructStageToggles()
         {
-            Text text;
-            UIFactory.CreateToggle(ContentRoot, name, out toggle, out text);
+            var columnObj = UIFactory.CreateUIObject("movesetColumn", _uiRoot);
+            UIFactory.SetLayoutGroup<VerticalLayoutGroup>(columnObj, childControlWidth: true, childControlHeight: true, forceWidth: true, forceHeight: false);
+            for (EWorldStage stage = EWorldStage.Stage1_1; stage < EWorldStage.StageSonic_1; stage++)
+            {
+                var debugUnlocksService = PMW2RPArchipelagoClientMod.services.ServiceFactory.GetDebugUnlocksService();
+                _constructToggle(columnObj, stage.ToString(), stage.ToString(), out Toggle toggle);
+                bool unlocked = debugUnlocksService.Stages.GetValueOrDefault(stage, false);
+                toggle.isOn = unlocked;
+                _stageToggles.Add(stage, toggle);
+            }
+        }
+
+        private void _constructToggle(GameObject parent, string name, string label, out Toggle toggle)
+        {
+            var toggleObj = UIFactory.CreateToggle(parent, name, out toggle, out Text text);
+            UIFactory.SetLayoutElement(toggleObj, minHeight: 10);
             text.text = label;
             text.fontSize = 24;
         }
 
         public void UIUpdate()
+        {
+            _updateMoveset();
+            _updateLevels();
+        }
+
+        private void _updateMoveset()
         {
             var debugUnlocksService = PMW2RPArchipelagoClientMod.services.ServiceFactory.GetDebugUnlocksService();
             debugUnlocksService.FlipKick = _kickToggle.isOn;
@@ -76,6 +111,15 @@ namespace PMW2RPArchipelagoClientDebugTools.ui
             else
             {
                 debugUnlocksService.ButtBounce = ProgressiveButtBounce.None;
+            }
+        }
+
+        private void _updateLevels()
+        {
+            var debugUnlocksService = PMW2RPArchipelagoClientMod.services.ServiceFactory.GetDebugUnlocksService();
+            for (EWorldStage stage = EWorldStage.Stage1_1; stage < EWorldStage.StageSonic_1; stage++)
+            {
+                debugUnlocksService.StagesMutable[stage] = _stageToggles[stage].isOn;
             }
         }
     }
